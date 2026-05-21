ee7f44d35b9f45724f07737844e71d3da6abc9bd
Lorenzo Bianco <lorenzobianco1098@gmail.com>
2026-05-21 09:15:04 +0200

    Added a rootlogon file to load and initialize lifetimeANA

---
+++ D0_lifetime/package/analysis/.rootlogon.C
@@ -0,0 +1,12 @@
+{
+    // 1. Carica il file con l'implementazione (assicurati che il percorso sia corretto)
+    // Puoi usare ".L lifetimeANA.C+" se desideri compilarlo con ACLiC, oppure solo ".L
+    // lifetimeANA.C"
+    gROOT->ProcessLine(".L lifetimeANA.C");
+
+    // 2. Dichiara l'oggetto globale 't' in modo che sia disponibile sulla riga di comando di ROOT
+    gROOT->ProcessLine("lifetimeANA t;");
+
+    // Opzionale: un messaggio di conferma
+    std::cout << "[D0_Tau] File lifetimeANA.C loaded and instance 't' created." << std::endl;
+}
